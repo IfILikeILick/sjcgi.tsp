8f30f28474fa9d182e20bf3ea04f44600a739104
ifilikeilick <if@cpt-ra.bid>
2025-04-15 12:36:20 +0100

    v0.3

--- sjengcgi.js
+++ sjengcgi.js
@@ -18,10 +18,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         ;
         ;
         const globje = (() => { }).constructor('return this;')();
-        //var once= 2;
+        //var once= 2; // ·•□■○◎●△▲☆★◇◆ ♥♠ ↑↓ ±× †‡ ·• ←↔→ ⇐⇔⇒
         const a_square = (idy) => (it_, idx) => {
+            const syms = '·•○●△▲ohnofu±×ck☆★□■◇◆itbrokesomehow';
             const it = it_.trim();
-            return String.fromCharCode(((idx + idy & 1 ? ' ' : '-') + it).slice(-1).charCodeAt(0) + (it.length !== 2 ? 0 : 32));
+            const alph = (((idx + idy & 1 ? ' ' : '-') + it).slice(-1).charCodeAt(0) + (it.length !== 2 ? 0 : 32)) & 127;
+            const alph2 = alph >> 5, alph5 = alph & 31;
+            const sym = syms.charAt(alph2 >> 1 ? (alph & 3 ? alph5 & 30 : 0) + alph2 : alph5 & 1);
+            /* console.log("It's a '"+sym+"': "+it_); /**/
+            return sym; // String.fromCharCode(sym);
         };
         function a_rank(it, idy) {
             return (it + it.slice(0, 2)).split('\|').map(a_square(idy)).join('&nbsp;');
@@ -46,8 +51,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
                 game += move + reply + '&';
                 field.value = '';
                 const board = gamma.slice(-9, -1).map(a_rank).join('<br />');
-                const edge1 = board.charCodeAt(0) & 1 ? '@ h g f e d c b a @' : '@ A B C D E F G H @';
-                const edge2 = board.charCodeAt(0) & 1 ? '@ H G F E D C B A @' : '@ a b c d e f g h @';
+                const edge1 = board.charCodeAt(0) & 1 ? '◎ h g f e d c b a ◎' : '◎ A B C D E F G H ◎';
+                const edge2 = board.charCodeAt(0) & 1 ? '◎ H G F E D C B A ◎' : '◎ a b c d e f g h ◎';
                 const rslt = edge1 + '<br />' + board + '<br />' + edge2;
                 if (elem)
                     elem.innerHTML = rslt;
@@ -60,15 +65,33 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
             at;
             const para1 = globje['para1_'];
             const text1 = globje['text1_'];
+            /**\
+         /* const qf= (x :string)=>(a_square(0)(x, 0));
+         
+         console.log("It's "+it
+           + qf(' ')  + qf('P')  + qf('b')
+           + qf('N')  + qf('r')  + qf('Q')
+           + qf('K')  + qf('k')  + qf('q')
+         ); /**/
             if (it.length >>> 1 !== 2) { }
             else {
-                const move = it + '&';
+                const move = it ? it + '&' : '';
                 const ante = "http://localhost/cgi-bin/sj5s.sh?d&force&st?5&";
                 const post = "go&&d&quit";
                 const prom = dostAsyncly(para1, text1, ante + game + move + post, move);
                 prom;
             }
         }
+        {
+            var _;
+            var _ = dostSyncly;
+            _;
+        } // typecheck
         globje['dostSyncly'] = dostSyncly;
     });
+    set_timeout(1230)(() => {
+        console.log("It's "
+            + (() => { }).constructor('return this;')()['dostSyncly']('', 'e2e4&e7e5'));
+    });
 })()); /**/
+;
