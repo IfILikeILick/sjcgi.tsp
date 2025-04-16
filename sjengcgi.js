6112701488600bfbf4ee6b28eb143e937854db57
ifilikeilick <if@cpt-ra.bid>
2025-04-16 09:34:55 +0100

    v0.5

--- sjengcgi.js
+++ sjengcgi.js
@@ -19,11 +19,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         ;
         ;
         const globje = (() => { }).constructor('return this;')();
-        // SYMS= " ·•□■○●◎◉△▲☆★◇◆ ⚪⚫ ♥♠♣ ↑↓ ±× †‡ ·• ←↔→ ⇐⇔⇒ ʘwʙxθɵ ʘ◎ʘ øʘθØø ♔♚♕♛♖♜♗♝♘♞♙♟"
+        // SYMS= " ·•□■○●◎◉△▲☆★◇◆ ☖☗ ♥♠♣ ↑↓ ±× †‡ ·• ←↔→ ⇐⇔⇒ ʘwʙxθɵ ʘ◎ʘ øʘθØø ♔♚♕♛♖♜♗♝♘♞♙♟"
         const the_squares_ = (pref, twice = 2 // syms= pref+'△▲ohnofu□■ck☆★○●◇◆itbrokesomehow_•_±×◎↑↓';
         ) => (idy // syms= pref+'♗♝○hn◉fu♔♚ck♘♞♕♛♖♜itbrokesomehow_·•●_±×◎↑↓';
         ) => (it_, idx) => {
-            const syms = pref + '△▲○hn●fu□■ck☆★⚪⚫◇◆itbrokesomehow_•_±×◎↑↓';
+            const syms = pref + '△▲○hn●fu☖☗ck☆★□■◇◆itbrokesomehow_•_±×◎↑↓';
             ;
             if (veracity) { }
             else if ((twice >>= 1) === 0) { }
