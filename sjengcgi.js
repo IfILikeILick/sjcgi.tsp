cb4b1b1a56048b6c0b5cf8b494934c488fac2cf6
ifilikeilick <if@cpt-ra.bid>
2025-04-27 18:28:56 +0100

    v0.8

--- sjengcgi.js
+++ sjengcgi.js
@@ -25,24 +25,160 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     const falsity = false;
     veracity;
     falsity;
+    function atPlay() { return (location.pathname) === '/play/'; } // atPlay;
     const set_timeout = (ms) => (fn, ...args) => setTimeout(fn, ms, ...args);
+    //
     set_timeout(0)(() => {
         //
-        set_timeout(101)(() => {
-            const goaw = globje;
-            const gdoc = goaw.document;
-            // Get a typed reference to the document on our globje:
-            //const documentObj = (globje as Window).document;
-            //const documentObj = (globje).document;
-            // Retrieve the paragraph element by its id:
-            const para1 = gdoc.getElementById("para1");
-            // Update the font size if the element exists:
-            if (para1) {
-                para1.style.fontSize = "300%"; // or any size string that you require
+        const decomposure = /(?:\/-?@?|-?\d+(?:\.\d+)?[-@]|-?[A-Za-z]\w*[-@]|-_\w*[-@]|_[-\.].*?_\._[-@])/g;
+        const decompose = (it, NO = '#NONONO:-') => (it.match(decomposure) || [NO + it]);
+        const precomposer = (it) => ('/' + ((it || '') + '/').replace(/\//g, '@/'));
+        const decofilter = (it, at, sa) => {
+            at;
+            sa; // discard "/..."
+            return it.charCodeAt(0) !== 47;
+        };
+        const decomposing = (it) => {
+            let q = 0;
+            for (let p = -1;;) {
+                const c = it.charCodeAt(++p) /**\ || 0 /**/; /* TODO= PICK 0 or NaN */
+                if (c !== 45 || p) {
+                    q = c << 1 | p;
+                    break;
+                }
             }
-        });
-        //;interface Obje { [key :string] :any; };
-        //const globje= ( ()=>{} ).constructor('return this;')() as Obje;
+            return q; // NB: The string itself is preserved (so, any leading hyphen remains in place)
+        };
+        const decomapping = (it, at, sa) => {
+            at;
+            sa;
+            let il = it.length, rv = NaN;
+            if (il) {
+                const cz = it.charCodeAt(--il); // NB: throws are somewhat inconsistent wrt length:-
+                if (cz === 45 || cz === 32 // TODO= USE; LOSE= '@'
+                    || cz === 64 // What about using 37, anyway?  SP would be best, I suppose?  Tricky one.
+                ) { }
+                else
+                    throw 'Not at 110 on 20250422 at 1744';
+                if (it.charCodeAt(0) !== 95) { }
+                else if ((il -= 3) >= 2) { } // throw invalid length:
+                else
+                    throw 'Not at 111 on 20250422 at 2241';
+                rv = decomposing(it);
+                it = it.substring(rv !== 190 ? 0 : 2, il); // *can* yield an empty string, ie not throw
+                sa[at] = it + '#' + rv;
+            }
+            return rv;
+        }; /**\
+        const f_out= function (this :number[], v :string, i :number, a :string[]) :void { v; a;
+          const t= this[i]; t;
+        };/**/
+        ;
+        const emptyobj = () => {
+            const n = {};
+            const s = {};
+            const rv = { _: n, __: s, it: null, };
+            return rv;
+        };
+        const f_in_ = (t_) => (rv, v, i, a) => {
+            a;
+            const t = t_[i];
+            if (!veracity) { }
+            else {
+                //let out_ :boolean;
+                const S = 1, C = 0, U = undefined, N = null;
+                S;
+                C;
+                if ((N >>> 0 & 1) >= 0) { }
+                else {
+                    throw 'Nope';
+                }
+                if ((U >>> 0 & 1) >= 0) { }
+                else {
+                    throw 'Nope';
+                }
+                //throw 'OK';
+            }
+            if (veracity) {
+                const pre = t >>> 0 & 1;
+                if (t === 190)
+                    rv.it = v;
+                else if ((t - 96 >>> 0) < 20 || t === 91)
+                    rv.it = 0 - parseFloat(v);
+                else if (t !== 191
+                    || v.length < 2 // TODO= LOSE
+                ) { }
+                else if ('number' === typeof rv.it) {
+                    rv._[v.substring(pre)] = rv.it;
+                }
+                else if ('string' === typeof rv.it) {
+                    rv.__['$' + v.substring(pre)] = rv.it;
+                }
+                else if (rv.it === null) { }
+                //       else if ('object' === (typeof (rv.it))) {}
+                else
+                    throw 'Not at 138 on 20250423 at 2343';
+            }
+            else
+                switch (0) {
+                } // TODO= LOSE tests, above
+            return rv;
+        };
+        const decomposer = (it) => {
+            it = precomposer(it);
+            const il = it.length;
+            const sa__ = decompose(it), sj = sa__.join(''), sl = sj.length;
+            //console.log("\n" + it.length + it + "\" -> " + JSON.stringify(sa) + " # = " + sl);
+            if (sl === il || !atPlay()) { }
+            else
+                throw 'Not at 121 on 20250422 at 1732'; // TODO?= better (ideally)
+            const a = sa__.filter(decofilter);
+            const t = a.map(decomapping);
+            //  a.forEach(f_out, t);
+            const rv = a.reduceRight(f_in_(t), emptyobj());
+            console.log('' + a.join('¬'));
+            return rv;
+        };
+        const patches = ''
+            + 'HALVE' + 'HH42' // +42
+            + 'HHUFF' + 'H032' // -32
+            //+ 'HULCE' + 'HH55' // -32 // TODO?= FIX or REPORT= not a string (which it isn't!)
+            + 'HHOPE' + 'HH3Q2808' // +3...
+            + 'HUQxyUQUQU' + 'HUQyzUQUQU' + 'HUQxyzUQUQU';
+        const patch4decomposer = ('/' + patches + '/')
+            .replace(/Q/g, '.').replace(/U/g, '_').replace(/H/g, '-');
+        const decomposer_patch = (path) => {
+            if (falsity)
+                return 'No stobje!';
+            else {
+                const it = decomposer(path !== "/play/" ? path : patch4decomposer);
+                //  const it :stobje_t_= it_; stobje= it;
+                stobje._ = it._;
+                stobje.__ = it.__;
+                return it;
+            }
+        };
+        (((path) => {
+            const it = decomposer_patch(path);
+            console.log("At 138 it's " + '\"' + JSON.stringify(it) + '\"');
+        })(location.pathname));
+        //
+    }); // END timeout
+    set_timeout(101)(() => {
+        //
+        if (atPlay()) { }
+        else
+            set_timeout(666)(() => {
+                const goaw = globje;
+                const gdoc = goaw.document;
+                const para1 = gdoc.getElementById("para1");
+                const percent = stobje._['_lOl'] || 300;
+                if (!para1) { }
+                else {
+                    ;
+                    para1.style.fontSize = '' + percent + '%';
+                }
+            }); // END inner timeout
         /*\ \**/ /**\ \**/ /**\ \**/
         ;
         ;
@@ -169,164 +305,35 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
                     + qf('N') + qf('r') + qf('Q')
                     + qf('K') + qf('k') + qf('q'));
             }
-            if (it.length >>> 1 !== 2) { }
-            else {
+            if (!text1) { }
+            else if (!para1) {
+                ; // TODO= erase?
+            }
+            else if (it.length >>> 1 !== 2) { }
+            else { // TODO= ||atPlay()
                 const move = it ? it + '&' : '';
                 const ante = "http://localhost/cgi-bin/sj5s.sh?d&force&st?5&";
                 const post = "go&&d&quit";
                 const prom = dostAsyncly(para1, text1, ante + game + move + post, move);
-                prom;
+                return prom;
             }
+            return null;
         }
         /*\ \**/ /**\ \**/ /**\ \**/
+        globje['dostSyncly'] = dostSyncly;
         if (veracity) { }
         else {
-            const _ = dostSyncly;
-            _;
+            const _out = dostSyncly;
+            _out;
         } // typecheck
-        globje['dostSyncly'] = dostSyncly;
+        console.log("At 281 it's an " + typeof ((() => { }).constructor('return this;')()['dostSyncly']('', 'e2e4')));
         //
-    }); // END 0ms
-    set_timeout(666)(() => {
-        //
-        var _a;
-        function atPlay() { return (location.pathname) === '/play/'; } // atPlay;
-        const decomposure = /(?:\/-?@?|-?\d+(?:\.\d+)?[-@]|-?[A-Za-z]\w*[-@]|-_\w*[-@]|_[-\.].*?_\._[-@])/g;
-        const decompose = (it, NO = '#NONONO:-') => (it.match(decomposure) || [NO + it]);
-        const precomposer = (it) => ('/' + ((it || '') + '/').replace(/\//g, '@/'));
-        const decofilter = (it, at, sa) => {
-            at;
-            sa; // discard "/..."
-            return it.charCodeAt(0) !== 47;
-        };
-        const decomposing = (it) => {
-            let q = 0;
-            for (let p = -1;;) {
-                const c = it.charCodeAt(++p) /**\ || 0 /**/; /* TODO= PICK 0 or NaN */
-                if (c !== 45 || p) {
-                    q = c << 1 | p;
-                    break;
-                }
-            }
-            return q; // NB: The string itself is preserved (so, any leading hyphen remains in place)
-        };
-        const decomapping = (it, at, sa) => {
-            at;
-            sa;
-            let il = it.length, rv = NaN;
-            if (il) {
-                const cz = it.charCodeAt(--il); // NB: throws are somewhat inconsistent wrt length:-
-                if (cz === 45 || cz === 32 // TODO= USE; LOSE= '@'
-                    || cz === 64 // What about using 37, anyway?  SP would be best, I suppose?  Tricky one.
-                ) { }
-                else
-                    throw 'Not at 110 on 20250422 at 1744';
-                if (it.charCodeAt(0) !== 95) { }
-                else if ((il -= 3) >= 2) { } // throw invalid length:
-                else
-                    throw 'Not at 111 on 20250422 at 2241';
-                rv = decomposing(it);
-                it = it.substring(rv !== 190 ? 0 : 2, il); // *can* yield an empty string, ie not throw
-                sa[at] = it + '#' + rv;
-            }
-            return rv;
-        }; /**\
-        const f_out= function (this :number[], v :string, i :number, a :string[]) :void { v; a;
-          const t= this[i]; t;
-        };/**/
-        ;
-        const emptyobj = () => {
-            const n = {};
-            const s = {};
-            const rv = { _: n, __: s, it: null, };
-            return rv;
-        };
-        const f_in_ = (t_) => (rv, v, i, a) => {
-            a;
-            const t = t_[i];
-            if (!veracity) { }
-            else {
-                //let out_ :boolean;
-                const S = 1, C = 0, U = undefined, N = null;
-                S;
-                C;
-                if ((N >>> 0 & 1) >= 0) { }
-                else {
-                    throw 'Nope';
-                }
-                if ((U >>> 0 & 1) >= 0) { }
-                else {
-                    throw 'Nope';
-                }
-                //throw 'OK';
-            }
-            if (veracity) {
-                const pre = t >>> 0 & 1;
-                if (t === 190)
-                    rv.it = v;
-                else if ((t - 96 >>> 0) < 20 || t === 91)
-                    rv.it = 0 - parseFloat(v);
-                else if (t !== 191
-                    || v.length < 2 // TODO= LOSE
-                ) { }
-                else if ('number' === typeof rv.it) {
-                    rv._[v.substring(pre)] = rv.it;
-                }
-                else if ('string' === typeof rv.it) {
-                    rv.__['$' + v.substring(pre)] = rv.it;
-                }
-                else if (rv.it === null) { }
-                //       else if ('object' === (typeof (rv.it))) {}
-                else
-                    throw 'Not at 138 on 20250423 at 2343';
-            }
-            else
-                switch (0) {
-                } // TODO= LOSE tests, above
-            return rv;
-        };
-        const decomposer = (it) => {
-            it = precomposer(it);
-            const il = it.length;
-            const sa__ = decompose(it), sj = sa__.join(''), sl = sj.length;
-            //console.log("\n" + it.length + it + "\" -> " + JSON.stringify(sa) + " # = " + sl);
-            if (sl === il || !atPlay()) { }
-            else
-                throw 'Not at 121 on 20250422 at 1732'; // TODO?= better (ideally)
-            const a = sa__.filter(decofilter);
-            const t = a.map(decomapping);
-            //  a.forEach(f_out, t);
-            const rv = a.reduceRight(f_in_(t), emptyobj());
-            console.log('' + a.join('¬'));
-            return rv;
-        };
-        const patches = ''
-            + 'HALVE' + 'HH42' // +42
-            + 'HHUFF' + 'H032' // -32
-            //+ 'HULCE' + 'HH55' // -32 // TODO?= FIX or REPORT= not a string (which it isn't!)
-            + 'HHOPE' + 'HH3Q2808' // +3...
-            + 'HUQxyUQUQU' + 'HUQyzUQUQU' + 'HUQxyzUQUQU';
-        const patch4decomposer = ('/' + patches + '/')
-            .replace(/Q/g, '.').replace(/U/g, '_').replace(/H/g, '-');
-        const decomposer_patch = (path) => {
-            if (falsity)
-                return 'No stobje!';
-            else {
-                const it = decomposer(path !== "/play/" ? path : patch4decomposer);
-                //  const it :stobje_t_= it_; stobje= it;
-                stobje._ = it._;
-                stobje.__ = it.__;
-                return JSON.stringify(it);
-            }
-        };
-        console.log("It's "
-            + ((_a = (() => { }).constructor('return this;')()['dostSyncly']('', 'e2e4')) !== null && _a !== void 0 ? _a : '\"' + decomposer_patch(location.pathname) + '\"'));
-        //
-    }); // END 666ms
+    }); // END timeout
     //
-})((() => { }).constructor('return this;')
-// FROM:= const globje= ( ()=>{} ).constructor('return this;')() as Obje;
-));
+})(// FROM:= const globje= ( ()=>{} ).constructor('return this;')() as Obje;
+(() => { }).constructor('return this;')
+//
+)); // END main
 //
 /*\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/
 //
