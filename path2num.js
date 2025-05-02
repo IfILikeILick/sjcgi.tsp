90918bb94f7fd49b5a7fccad649944d6f652c002
ifilikeilick <if@cpt-ra.bid>
2025-05-02 19:07:21 +0100

    v0.9

--- path2num.js
+++ path2num.js
@@ -14,7 +14,8 @@
     veracity;
     falsity;
     function atPlay() { return (pathname) === '/play/'; } // atPlay;
-    const set_timeout = (ms) => (fn, ...args) => setTimeout(fn, ms, ...args);
+    const set_timeout = (ms) => (fn, ...args // TODO= RESEARCH
+    ) => setTimeout(fn, ms, ...args);
     //
     set_timeout(0)(() => {
         //
@@ -75,6 +76,8 @@
             const f_out= function (this :number[], v :string, i :number, a :string[]) :void { v; a;
               const t= this[i]; t;
             };/**/
+            ;
+            ;
             ;
             const emptyobj = () => {
                 const n = {};
@@ -175,6 +178,7 @@
              (((idealName, libToAdd) => {
                 ;
                 ;
+                ;
                 const globje = (() => { }).constructor('return this;')();
                 if (globje['NO_' + idealName]) { }
                 else {
@@ -204,26 +208,84 @@
         _._._._._._._._._._._._._._._._;
         ((($) => {
             /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
-            const stillBest = (best, test) => ((b, t, d) => (d < 0 || !d && b < t))(best, test, test.length - best.length) ? best : test;
-            const myName___ = (name, obje = globje) => Object.keys(obje)
+            const theBest = (best, test) => ((b, t, d) => (d < 0 || !d && b < t))(best, test, test.length - best.length) ? best : test;
+            const aName__ = (name, obje = globje) => Object.keys(obje)
                 .filter((key) => 1 + key.search(new RegExp(name + '[$_]*')))
-                .reduce(stillBest);
-            const myName__ = (genus, obje = globje) => (species) => myName___(genus + '_' + species, obje);
-            const myName_ = myName__('NML11th');
-            const myName = myName_('LithePath');
+                .reduce(theBest);
+            const aName_ = (genus, obje = globje) => (species) => aName__(genus + '_' + species, obje);
+            const aName = aName_('NML11th');
+            //const myName= myName_('LithePath');
             if (!always) { }
             else
                 console.log('\n'
-                    + JSON.stringify(myName) + '\n'
+                    + JSON.stringify(aName('LithePath')) + '\n'
                     + JSON.stringify($) + '\n'
                     + JSON.stringify($.pathObje()) + '\n');
+            //for (let i= 44, j= 1; --i; ) console.log(''+(j= (j>>>0<<1>>>0|j>>>1)));
+            //for (let i= 4, j= 1; --i; ) console.log(''+(j= (j>>>0<<1>>>0|j>>>1)>>>0));
+            //for (let i= 44, j= 0; --i; ) console.log(''+(j= ((j<<1>>>0|1)>>>0)+(j>>>1)));
+            ((() => {
+                const throwNaN = (whether // TODO= PICK= always throw?????
+                ) => (it) => {
+                    if (!whether) { }
+                    else
+                        throw 'NaN/risk on 20250430 at 2005';
+                    return it !== null && it !== void 0 ? it : NaN;
+                };
+                const NaN_ = throwNaN(false);
+                NaN_;
+                const us01526Factory = (sh) => {
+                    const k = sh & 31; // or 31 if need >25 days(!)
+                    return (it) => ((it += (it >>> k | 1) >>> 0) < (-1 >>> 0) ? it : NaN_());
+                };
+                const strive = (dost, latest4species, ms16_, globje_ = globje) => (get_) => {
+                    let ms16 = 0;
+                    const f = (g) => () => {
+                        const got = latest4species(get_);
+                        console.log('Loaded ' + got + ' after ' + (ms16 >> 16) + 'ms');
+                        if (got)
+                            dost(globje_[got]);
+                        else
+                            set_timeout((ms16 = ms16_(ms16)) >> 16)(g(g));
+                    };
+                    const ff = f(f);
+                    ff();
+                };
+                /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+                function go(it) {
+                    const g = us01526Factory(it);
+                    let i, j = 0;
+                    const k = 999999;
+                    let t = 0;
+                    for (i = k; !!(j = g(j)) && --i;)
+                        t += j >>> 16;
+                    if (i) { }
+                    else if (falsity)
+                        throw 'Excessive iter.';
+                    else
+                        return +Infinity;
+                    return /**/ (t / 60 >>> 0) / 1000.0; /*/ k-i /**/
+                    ;
+                }
+                //  console.log('#1= '+((()=>{ const n= -1, p= n>>>1; return n>>>0|p; })()));
+                console.log('#2=..' + ((() => {
+                    let rv = '';
+                    for (let i = 19; --i >= 0;)
+                        rv += ', ' + i + ': ' + go(i);
+                    return rv;
+                })()));
+                function st(it) {
+                    console.log(JSON.stringify(it.pathObje()));
+                }
+                strive(st, aName, us01526Factory(3))('LithePath');
+            })());
             /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
             if (!!always) { }
             else // IIFE
                 (((test) => {
                     const _ = undefined;
                     _;
-                    console.log("It's " + test);
+                    console.log("It's a " + test);
                 })('test'));
             /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
             /*  END vectors  */ _._._._._._._._._._._._._._._._._._._;
---
+++ rock4nml.js
@@ -0,0 +1,167 @@
+"use strict";
+;
+;
+(((globje_) => {
+    const globje = globje_();
+    globje;
+    const pathname = globje['location'].pathname;
+    const veracity = true;
+    const falsity = false;
+    veracity;
+    falsity;
+    function atPlay() { return (pathname) === '/play/'; }
+    ;
+    atPlay;
+    const set_timeout = (ms) => (fn, ...args // TODO= RESEARCH
+    ) => setTimeout(fn, ms, ...args);
+    //
+    set_timeout(0)(() => {
+        //
+        ;
+        ;
+        const _ = { _: undefined, };
+        _._ = _;
+        const always = true;
+        always; // const never= false; never;
+        //
+        /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+        /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+        /*  THE library  */ ;
+        _._._._._._._._._._._._._._._._._._;
+        const atNow = ((() => {
+            /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+            const theBest = (best, test) => ((b, t, d) => (d < 0 || !d && b < t))(best, test, test.length - best.length) ? best : test;
+            const theName = (name, obje = globje) => Object.keys(obje)
+                .filter((key) => 1 + key.search(new RegExp(name + '[$_]*')))
+                .reduce(theBest);
+            const gloName = (genus, obje = globje) => (species) => theName(genus + '_' + species, obje);
+            const nmlName = gloName('NML11th');
+            /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+            const throwNaN = (whether = true // TODO= PICK= really, always throw??
+            ) => (it) => {
+                if (!whether) { }
+                else
+                    throw 'NaN/risk on 20250430 at 2005';
+                return it !== null && it !== void 0 ? it : NaN;
+            };
+            // const NaN_= throwNaN(false); NaN_;
+            const _15ns26sequence = (sh, wh = false) => {
+                const nan = throwNaN(wh);
+                const k = sh & 31; // not 15?
+                return (it) => ((it += (it >>> k | 1) >>> 0) < (-1 >>> 0) ? it : nan());
+            };
+            const waitFor = (dost, latest4species, ms16_, globje_ = globje) => (get_) => {
+                let ms16 = 0;
+                const f = (g) => () => {
+                    const got = latest4species(get_);
+                    console.log('Loaded ' + got + ' after ' + (ms16 >> 16) + 'ms');
+                    if (got)
+                        dost(globje_[got]);
+                    else
+                        set_timeout((ms16 = ms16_(ms16)) >> 16)(g(g));
+                };
+                const ff = f(f);
+                ff();
+            };
+            /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+            const theLava = // IIFE
+             ((() => {
+                return {
+                    throwNaN, _15ns26sequence
+                };
+            })());
+            const toExport = // IIFE
+             ((() => {
+                return {
+                    atPlay, set_timeout, gloName, nmlName, waitFor, theLava,
+                };
+            })());
+            const addedLib = // IIFE
+             (((idealName, libToAdd) => {
+                ;
+                ;
+                ;
+                const globje = (() => { }).constructor('return this;')();
+                if (globje['NO_' + idealName]) { }
+                else {
+                    let counter = 0 - 1; /**/
+                    const suffix = () => (++counter ?
+                        ('0' + counter.toString(2)).replace(/0/g, '_').replace(/1/g, '$') :
+                        '_');
+                    let hopedName = idealName;
+                    while ('undefined' !== typeof globje[hopedName])
+                        hopedName = idealName + suffix();
+                    globje[hopedName] = libToAdd;
+                    console.log("Loaded " + idealName + " as " + hopedName);
+                }
+                return libToAdd;
+            })('NML11th_LitheRock', toExport)); /**/
+            /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+            /*  END library  */ _._._._._._._._._._._._._._._._._;
+            return addedLib;
+        })());
+        /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+        /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+        /*  THE vectors  */ ;
+        _._._._._._._._._._._._._._._._;
+        ((($) => {
+            /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+            const $$ = $.theLava;
+            if (!always) { }
+            else
+                console.log('\n'
+                    + JSON.stringify($.nmlName('LitheRock')) + '!\n'
+                    + JSON.stringify($.nmlName('LithePath')) + '!\n'
+                    + JSON.stringify('' + $$.throwNaN(!1)()) + '!\n');
+            ((() => {
+                function dur(it) {
+                    const g = $.theLava._15ns26sequence(it);
+                    let j = 0;
+                    const k = 999999;
+                    let i, t = 0;
+                    for (i = k; !!(j = g(j)) && --i;)
+                        t += j >>> 16;
+                    if (i) { }
+                    else if (falsity)
+                        throw 'Excessive iter.';
+                    else
+                        return +Infinity;
+                    return /**/ (t / 60 >>> 0) / 1000.0; /*/ k-i /**/
+                    ;
+                }
+                //for (let i= 44, j= 1; --i; ) console.log(''+(j= (j>>>0<<1>>>0|j>>>1)));
+                //for (let i= 4, j= 1; --i; ) console.log(''+(j= (j>>>0<<1>>>0|j>>>1)>>>0));
+                //for (let i= 44, j= 0; --i; ) console.log(''+(j= ((j<<1>>>0|1)>>>0)+(j>>>1)));
+                //  console.log('#1= '+((()=>{ const n= -1, p= n>>>1; return n>>>0|p; })()));
+                console.log('#2=..' + ((() => {
+                    let rv = '';
+                    for (let i = 19; --i >= 0;)
+                        rv += ', ' + i + ': ' + dur(i);
+                    return rv;
+                })()));
+                function st(it) {
+                    console.log(JSON.stringify(it.theLava.throwNaN(true)()));
+                }
+                $.waitFor(st, $.nmlName, $$._15ns26sequence(3))('LitheRock'); // TODO?= Path?
+            })());
+            /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+            if (!!always) { }
+            else // IIFE
+                (((test) => {
+                    ;
+                    console.log("It's still just a " + test);
+                })('test'));
+            /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+            /*  END vectors  */ _._._._._._._._._._._._._._._._._._._;
+            return;
+        })(atNow));
+        /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+        /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**\ \**/ /**75**/
+        //
+    }); // END 0ms timeout
+    //
+})(// FROM:= const globje= ( ()=>{} ).constructor('return this;')() as Obje;
+(() => { }).constructor('return this;')
+//
+)); // END outermost
+//
