55b1139e0230f7c601baa8d2dbaeb95e5ad4578e
ifilikeilick <if@cpt-ra.bid>
2025-05-11 01:29:06 +0100

    v0.9c

--- sjengcgi.js
+++ sjengcgi.js
@@ -1,5 +1,5 @@
-"use strict";
 /**\ vim: set syntax=typescript :miv -+- very alpha MATE -:= /**/
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
